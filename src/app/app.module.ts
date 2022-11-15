35fb7cc02632fd7836e7e48153f08ed7d4ebd7fc
James Ferry <jamesferry84@hotmail.com>
2022-11-15 13:42:49 +0000

    master to contain barebones

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,26 +3,10 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import {FormsModule} from "@angular/forms";
-import { ShoppingComponent } from './shopping/shopping.component';
-import { ShoppingListComponent } from './shopping/shopping-list/shopping-list.component';
-import { ShoppingEditComponent } from './shopping/shopping-edit/shopping-edit.component';
-import { RecipeComponent } from './recipe/recipe.component';
-import { RecipeListComponent } from './recipe/recipe-list/recipe-list.component';
-import { RecipeItemComponent } from './recipe/recipe-item/recipe-item.component';
-import { RecipeDetailComponent } from './recipe/recipe-detail/recipe-detail.component';
-import { HeaderComponent } from './header/header.component';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    ShoppingComponent,
-    ShoppingListComponent,
-    ShoppingEditComponent,
-    RecipeComponent,
-    RecipeListComponent,
-    RecipeItemComponent,
-    RecipeDetailComponent,
-    HeaderComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
