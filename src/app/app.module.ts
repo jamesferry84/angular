96bfb702737ac7eb96c061e52dee9e6d2ac13c38
James Ferry <jamesferry84@hotmail.com>
2022-11-16 12:49:50 +0000

    added routes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,26 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
 
-import { AppComponent } from './app.component';
+import {AppComponent} from './app.component';
 import {FormsModule} from "@angular/forms";
-import { ShoppingComponent } from './shopping/shopping.component';
-import { ShoppingListComponent } from './shopping/shopping-list/shopping-list.component';
-import { ShoppingEditComponent } from './shopping/shopping-edit/shopping-edit.component';
-import { RecipeComponent } from './recipe/recipe.component';
-import { RecipeListComponent } from './recipe/recipe-list/recipe-list.component';
-import { RecipeItemComponent } from './recipe/recipe-item/recipe-item.component';
-import { RecipeDetailComponent } from './recipe/recipe-detail/recipe-detail.component';
-import { HeaderComponent } from './header/header.component';
+import {ShoppingComponent} from './shopping/shopping.component';
+import {ShoppingListComponent} from './shopping/shopping-list/shopping-list.component';
+import {ShoppingEditComponent} from './shopping/shopping-edit/shopping-edit.component';
+import {RecipeComponent} from './recipe/recipe.component';
+import {RecipeListComponent} from './recipe/recipe-list/recipe-list.component';
+import {RecipeItemComponent} from './recipe/recipe-item/recipe-item.component';
+import {RecipeDetailComponent} from './recipe/recipe-detail/recipe-detail.component';
+import {HeaderComponent} from './header/header.component';
 import {BasicHighlightDirective} from "./directives/basic-highlight.directive";
 import {DropdownDirective} from "./directives/dropdown.directive";
+import {RouterModule, Routes} from "@angular/router";
+import { HomeComponent } from './home/home.component';
+
+const appRoutes: Routes = [
+  {path: '', component: HomeComponent},
+  {path: 'recipes', component: RecipeComponent},
+  {path: 'shopping', component: ShoppingComponent}
+];
 
 @NgModule({
   declarations: [
@@ -26,13 +34,16 @@ import {DropdownDirective} from "./directives/dropdown.directive";
     RecipeDetailComponent,
     HeaderComponent,
     BasicHighlightDirective,
-    DropdownDirective
+    DropdownDirective,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
-    FormsModule
+    FormsModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
--- src/app/recipe/recipe-detail/recipe-detail.component.ts
+++ src/app/recipe/recipe-detail/recipe-detail.component.ts
@@ -1,13 +1,17 @@
 import { Component, OnInit } from '@angular/core';
+import {LoggingService} from "../../services/logging.service";
 
 @Component({
   selector: 'app-recipe-detail',
   templateUrl: './recipe-detail.component.html',
-  styleUrls: ['./recipe-detail.component.css']
+  styleUrls: ['./recipe-detail.component.css'],
+  providers: [LoggingService]
 })
 export class RecipeDetailComponent implements OnInit {
 
-  constructor() { }
+  constructor(private loggingService: LoggingService) {
+    this.loggingService.logToConsole('recipe detail component created with logging service');
+  }
 
   ngOnInit(): void {
   }
