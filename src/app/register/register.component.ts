08e6c66f1480ae92b28e49c8952a4d9b149a40ac
James Ferry <jamesferry84@hotmail.com>
2022-11-17 11:46:20 +0000

    added forms

--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -1,5 +1,5 @@
-import { Component, OnInit } from '@angular/core';
-import {FormArray, FormControl, FormGroup, Validator, Validators} from "@angular/forms";
+import {Component, OnInit} from '@angular/core';
+import {FormArray, FormControl, FormGroup, Validators} from "@angular/forms";
 import {Observable} from "rxjs";
 
 @Component({
@@ -19,15 +19,35 @@ export class RegisterComponent implements OnInit {
     this.registerForm = new FormGroup({
       'userData': new FormGroup({
         'username': new FormControl(null, [Validators.required, this.forbiddenNames.bind(this)]),
-        'email': new FormControl(null, [Validators.required, Validators.email]),
+        'email': new FormControl(null, [Validators.required, Validators.email], this.forbiddenEmails.bind(this)),
       }),
       'gender': new FormControl('male'),
       'hobbies': new FormArray([])
-    })
+    });
+    // this.registerForm.valueChanges.subscribe((value) => {
+    //   console.log(value);
+    // })
+    // this.registerForm.statusChanges.subscribe((value) => {
+    //   console.log(value);
+    // })
+    // this.registerForm.setValue({
+    //   'userData': {
+    //     'username': 'James',
+    //     'email': 'james@james.com'
+    //   },
+    //   'gender': 'male',
+    //   'hobbies': []
+    // });
+    this.registerForm.patchValue({
+      'userData': {
+        'username': 'Tester'
+      }
+    });
   }
 
   onSubmit() {
     console.log(this.registerForm);
+    this.registerForm.reset();
   }
 
   onAddHobby() {
@@ -43,11 +63,15 @@ export class RegisterComponent implements OnInit {
   }
 
   forbiddenEmails(control: FormControl): Promise<any> | Observable<any> {
-    const promise = new Promise<any>((resolve, reject) => {
+    return new Promise<any>((resolve, reject) => {
       setTimeout(() => {
-
+        if (control.value === 'test@test.com') {
+          resolve({'emailIsForbidden': true});
+        } else {
+          resolve(null);
+        }
       }, 1500);
-    })
+    });
   }
 
 }
