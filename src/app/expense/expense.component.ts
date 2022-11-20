aa853d48001e554ed6a01c5f27d760d8e5b18d63
jamesferry84 <jamesferry84@hotmail.com>
2022-11-20 14:02:16 +0000

    macbook updates

--- src/app/expense/expense.component.ts
+++ src/app/expense/expense.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {Expense} from "../../models/expense";
 
 @Component({
   selector: 'app-expense',
@@ -7,9 +8,23 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ExpenseComponent implements OnInit {
 
-  constructor() { }
+  showForm: boolean = false;
+
+  dummyExpenseData: Expense[] = [
+    { id: 1, name: 'Mortgage', amount: 800, frequency: 'monthly'},
+    { id: 2, name: 'Council Tax', amount: 330, frequency: 'monthly'},
+    { id: 3, name: 'Kitchen', amount: 550, frequency: 'monthly'},
+    { id: 4, name: 'gas / electric', amount: 400, frequency: 'monthly'},
+  ];
+
+  constructor() {
+  }
 
   ngOnInit(): void {
+  }
+
+  showEditForm() {
+    this.showForm = !this.showForm;
   }
 
 }
---
+++ src/models/expense.ts
@@ -0,0 +1,7 @@
+export class Expense {
+  id?: number;
+  name: string;
+  amount: number;
+  frequency: string;
+  expire?: Date;
+}
